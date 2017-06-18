85e8c7d74b67cc912ed5010dc164eff9c5d17704
bookstore <booookstore@icloud.com>
2017-06-18 22:17:13 +0900

    [Add] カテゴリーに基づく検索と、テストケースを追加

--- src/main/java/sample/springboot/repository/EventRepository.java
+++ src/main/java/sample/springboot/repository/EventRepository.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
+import sample.springboot.entity.Category;
 import sample.springboot.entity.Event;
 
 public interface EventRepository extends CrudRepository<Event, Integer> {
 	
+	/** イベントタイトルから、イベントエンティティを取得 */
 	public List<Event> findEventByTitle(String title);
 
+	public List<Event> findEventByCategory(Category category);
 }
--- src/test/java/sample/springboot/repository/EventRepositoryTest.java
+++ src/test/java/sample/springboot/repository/EventRepositoryTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -39,6 +40,7 @@ public class EventRepositoryTest {
 	 */
 	@Before
 	public void setUp() {
+		eventReository.deleteAll();
 		categoryRepository.deleteAll();
 
 		// カテゴリーを登録
@@ -61,12 +63,52 @@ public class EventRepositoryTest {
 	 */
 	@Test
 	public void testFindEventByTitle() {
-		
+
 		List<Event> result = eventReository.findEventByTitle("Perfume");
-		
+
 		// 検証
 		assertThat(result.size()).isEqualTo(1);
 		assertThat(result).extracting("title").containsOnly("Perfume");
+	}
+
+	/**
+	 * カテゴリーからイベントエンティティを取得する。
+	 */
+	@Test
+	public void testFindEventByCategory() {
+
+		Iterable<Category> categories = categoryRepository.findAll();
+		Category category = categories.iterator().next();
+
+		// 検証
+		List<Event> result = eventReository.findEventByCategory(category);
+		assertThat(result)
+				.extracting(e -> e.getCategory().getName())
+				.containsOnly(category.getName());
+	}
+
+	/**
+	 * フェッチしたオブジェクトを変更しても、データベースへは登録されない。
+	 * フェッチしたオブジェクトを変更し、再度登録するとデータベースが更新される。
+	 */
+	@Test
+	public void testFetchedObjectModify() {
+		
+		// タイトルが Perfume のイベントを取得し、タイトルを 赤い公園 へ変更
+		Event perfumeEvent = eventReository.findEventByTitle("Perfume").get(0);
+		perfumeEvent.setTitle("赤い公園");
+
+		// DBへ登録していないため、DBは変更なし
+		assertThat(eventReository.findEventByTitle("Perfume").get(0)).isNotNull();
+		assertThat(eventReository.findEventByTitle("赤い公園").size()).isEqualTo(0);
+		
+		// イベントタイトルを変更
+		perfumeEvent.setTitle("赤い公園");
+		
+		// DBへ登録したため、DBが更新される。
+		eventReository.save(perfumeEvent);
+		assertThat(eventReository.findEventByTitle("Perfume").size()).isEqualTo(0);
+		assertThat(eventReository.findEventByTitle("赤い公園").get(0)).isNotNull();
 	}
 
 }
